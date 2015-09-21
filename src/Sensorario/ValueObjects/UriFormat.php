e869f1615869b1095e023c586a23a2d90da735c2
Simone Gentili <sensorario@gmail.com>
2015-09-21 15:14:46 +0200

    UriFormat request Value Object

---
+++ src/Sensorario/ValueObjects/UriFormat.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Sensorario\ValueObjects;
+
+use Sensorario\ValueObject\ValueObject;
+
+final class UriFormat extends ValueObject
+{
+    public static function allowed()
+    {
+        return [
+            'query',
+            'fragment',
+        ];
+    }
+
+    public static function mandatory()
+    {
+        return [
+            'scheme',
+            'authority',
+            'path',
+        ];
+    }
+
+    public function value()
+    {
+        if ($this->properyExists('query')) {
+            return $this->scheme() . '://'
+                . $this->authority()
+                . $this->path()
+                . '?' . $this->query()
+            ;
+        }
+
+        return $this->scheme() . '://'
+            . $this->authority()
+            . $this->path()
+        ;
+    }
+}
---
+++ test/unit/Sensorario/ValueObjects/UriFormatTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Sensorario\ValueObjects;
+
+use PHPUnit_Framework_TestCase;
+
+final class UriFormatTest extends PHPUnit_Framework_TestCase
+{
+    public function testGenericURISyntax()
+    {
+        $uri = UriFormat::create([
+            'scheme'    => 'http',
+            'authority' => 'www.example.com',
+            'path'      => '/path/to/resource',
+        ]);
+
+        $this->assertEquals(
+            'http://www.example.com/path/to/resource',
+            $uri->value()
+        );
+    }
+
+    public function testUriWithQuery()
+    {
+        $uri = UriFormat::create([
+            'scheme'    => 'http',
+            'authority' => 'www.example.com',
+            'path'      => '/path/to/resource',
+            'query'     => 'foo=bar&hello=world',
+        ]);
+
+        $this->assertEquals(
+            'http://www.example.com/path/to/resource?foo=bar&hello=world',
+            $uri->value()
+        );
+    }
+}
